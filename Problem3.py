7dcc60438388d959ba261c33f6b782002904976c
Mateusz Soltysik <matek92@gmail.com>
2012-08-17 23:58:23 +0200

    [Python]Problem3

--- Problem3.py
+++ Problem3.py
@@ -1,15 +1,20 @@
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
 # -- coding: UTF-8 --
 __author__ = 'Mateusz'
+
 
 def Problem3(number):
     tab = []
     startnumber = 2
-    while (number > 1):
+    while number > 1:
         while (number % startnumber == 0):
             tab.append(startnumber)
             number /= startnumber
         startnumber += 1
-    return tab
+    return tab[-1]
 
-
-print Problem3(600851475143)[-1]
+print Problem3(600851475143)
